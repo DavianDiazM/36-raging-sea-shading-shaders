2f4e847156e69f747b81bb942e628c81af18ef90
Davian Diaz <daviandiaz5@gmail.com>
2024-09-18 08:43:23 -0400

    First Commit

---
+++ src/shaders/water/fragment.glsl
@@ -0,0 +1,41 @@
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
+
+void main()
+{
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    mixStrength = smoothstep(0.0, 1.0, mixStrength);
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+
+    vec3 light = vec3(0.0);
+
+    light += pointLight(
+        vec3(1.0),
+        10.0,
+        normal,
+        vec3(0.0, 0.25, 0.0),
+        viewDirection,
+        30.0,
+        vPosition,
+        0.95
+    );
+    
+    color *= light;
+    
+    gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
---
+++ src/shaders/water/vertex.glsl
@@ -0,0 +1,63 @@
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+
+varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/perlinClassic3D.glsl
+
+float waveElevation(vec3 position)
+{
+    float elevation = sin(position.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                      sin(position.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+                      uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++)
+    {
+        elevation -= abs(perlinClassic3D(vec3(position.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }
+
+    return elevation;
+}
+
+void main()
+{
+    // Base position
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float shift = 0.01;
+    vec3 modelPositionA = modelPosition.xyz + vec3(shift, 0.0, 0.0);
+    vec3 modelPositionB = modelPosition.xyz + vec3(0.0, 0.0, - shift);
+
+    // Elevation
+    float elevation = waveElevation(modelPosition.xyz);
+    float elevationA = waveElevation(modelPositionA);
+    float elevationB = waveElevation(modelPositionB);
+    
+    modelPosition.y += elevation;
+    modelPositionA.y += elevationA;
+    modelPositionB.y += elevationB;
+
+    // Compute normal
+    vec3 toA = normalize(modelPositionA - modelPosition.xyz);
+    vec3 toB = normalize(modelPositionB - modelPosition.xyz);
+    vec3 computedNormal = cross(toA, toB);
+
+    // Final position
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // Varyings
+    vElevation = elevation;
+    vNormal = computedNormal;
+    vPosition = modelPosition.xyz;
+}
